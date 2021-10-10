e8f10c5b3f792586f1f7c8a0902b52cc7dc75933
syafiqhd <syafiqhd2001@gmail.com>
2021-10-10 17:03:32 +0700

    initial commit

---
+++ AssetsProblem-5/Scripts/CircleInputMouse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleInputMouse : MonoBehaviour
+{
+    public Transform point;
+    private Rigidbody2D rigidBody2D;
+    private Vector3 cursorPos;
+    private Vector3 direction;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        point.position = cursorPos;
+    }
+
+    void Update()
+    {
+        cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (cursorPos - transform.position).normalized;
+
+        PushBall(direction);
+    }
+
+    void PushBall(Vector3 dir)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidBody2D.velocity = new Vector2(dir.x * 15, dir.y * 15);
+        }
+    }
+}
