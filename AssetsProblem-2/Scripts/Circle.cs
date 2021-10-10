99d8f4cf1594a8eaa366860adda397b2bb663ebd
syafiqhd <syafiqhd2001@gmail.com>
2021-10-10 16:45:03 +0700

    initial commit

---
+++ AssetsProblem-2/Scripts/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    public float xInitialForce;
+    public float yInitialForce;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        Invoke("PushBall", 0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void PushBall()
+    {
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float randomDirection = Random.Range(0, 2);
+
+        if (randomDirection < 1.0f)
+        {
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+}
