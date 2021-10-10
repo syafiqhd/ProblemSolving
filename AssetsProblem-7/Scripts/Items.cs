a6a660410196d6224c523fe450cabcb104ce882d
syafiqhd <syafiqhd2001@gmail.com>
2021-10-10 17:07:18 +0700

    initial commit

---
+++ AssetsProblem-7/Scripts/Items.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.score++;
+            GameManager.itemCount--;
+            Destroy(gameObject);
+        }
+    }
+}
